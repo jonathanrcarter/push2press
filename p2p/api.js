91ad83f64ea6630608c9b661f7de015d9509ae3b
Jonathan Carter <jc@glimworm.com>
2013-03-06 12:15:37 +0100

    add user voice

--- p2p/api.js
+++ p2p/api.js
@@ -132,3 +132,11 @@ $(function() {
 	$("#sortable").disableSelection();
 });
 
+
+var uvOptions = {};
+(function() {
+	var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+	uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/wlHMAlKuZ4MLJmN2U2w2RA.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
+
