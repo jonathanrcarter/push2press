f47601dd6c75d0093e3564e0f61f3f8dfca2e369
Jonathan Carter <jc@glimworm.com>
2013-03-06 16:27:35 +0100

    wordpress plugin changes, adding paths to install and upgrade scripts

--- p2p/install.php
+++ p2p/install.php
@@ -46,14 +46,14 @@ echo "<img src='http://www.push2press.com/p2p/images/application-logo.png'></p>"
 
 echo "<p>downloading version from github ... success</p>";
 
-$download = file_put_contents("upgrade.zip", file_get_contents("https://github.com/jonathanrcarter/push2press/archive/master.zip"));
+$download = file_put_contents(dirname(__FILE__)."/p2p/upgrade.zip", file_get_contents("https://github.com/jonathanrcarter/push2press/archive/master.zip"));
 
 if ($download == false) {
 	echo "exiting, error downloading code";
 	exit;
 }
 
-$download = file_put_contents("pclzip.lib.php", file_get_contents("https://raw.github.com/jonathanrcarter/push2press/master/p2p/pclzip-2-8-2/pclzip.lib.php"));
+$download = file_put_contents(dirname(__FILE__)."/p2p/pclzip.lib.php", file_get_contents("https://raw.github.com/jonathanrcarter/push2press/master/p2p/pclzip-2-8-2/pclzip.lib.php"));
 
 if ($download == false) {
 	echo "exiting, error downloading code";
@@ -62,18 +62,21 @@ if ($download == false) {
 
 
 require_once('pclzip.lib.php');
-$archive = new PclZip('upgrade.zip');
+$archive = new PclZip(dirname(__FILE__).'/p2p/upgrade.zip');
 
 //$list = $archive->listContent();
 //var_dump($list);
 
-if ($archive->extract(PCLZIP_OPT_REMOVE_PATH, "push2press-master/p2p",PCLZIP_OPT_REPLACE_NEWER) == 0) {
+if ($archive->extract(PCLZIP_OPT_PATH, dirname(__FILE__)."/p2p", PCLZIP_OPT_REMOVE_PATH, "push2press-master/p2p",PCLZIP_OPT_REPLACE_NEWER) == 0) {
 	echo "exiting, error downloading code";
 	exit;
 }
 
-
-echo "<div><a class='btn btn-success' href='api.php'>You can proceed to set up your site by clicking here</a></div>";
+if ($hosted && $hosted == "wordpress") {
+	echo "<div><a class='btn btn-success' href='/wp-admin/admin.php?page=push2press/admin.php'>You can proceed to set up your site by clicking here</a></div>";
+} else {
+	echo "<div><a class='btn btn-success' href='api.php'>You can proceed to set up your site by clicking here</a></div>";
+}
 
 echo $hbot;
 
--- p2p/upgrade.php
+++ p2p/upgrade.php
@@ -87,7 +87,7 @@ echo "<br>";
 
 echo "<p>downloading version from github ... success</p>";
 
-$download = file_put_contents("upgrade.zip", file_get_contents("https://github.com/jonathanrcarter/push2press/archive/master.zip"));
+$download = file_put_contents(dirname(__FILE__)."/upgrade.zip", file_get_contents("https://github.com/jonathanrcarter/push2press/archive/master.zip"));
 
 if ($download == false) {
 	echo "exiting, error downloading code";
@@ -95,12 +95,12 @@ if ($download == false) {
 }
 
 require_once('pclzip-2-8-2/pclzip.lib.php');
-$archive = new PclZip('upgrade.zip');
+$archive = new PclZip(dirname(__FILE__).'/upgrade.zip');
 
 //$list = $archive->listContent();
 //var_dump($list);
 
-if ($archive->extract(PCLZIP_OPT_REMOVE_PATH, "push2press-master/p2p",PCLZIP_OPT_REPLACE_NEWER) == 0) {
+if ($archive->extract(PCLZIP_OPT_PATH, dirname(__FILE__) ,PCLZIP_OPT_REMOVE_PATH, "push2press-master/p2p",PCLZIP_OPT_REPLACE_NEWER) == 0) {
 	echo "exiting, error downloading code";
 	exit;
 }
