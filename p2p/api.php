78d20b44aeee7e6ac42ef03846da0c4ee1acf8d2
Jonathan Carter <jc@glimworm.com>
2013-03-07 10:27:53 +0100

    email sending error

--- p2p/api.php
+++ p2p/api.php
@@ -3256,6 +3256,7 @@ echo "<!--\n\n $query; \n\n-->";
 		
 		if ($emaillinkto != "") {
 			$message = sprintf("Link to push2press page is <a href=\"push2press://?url=%s\">%s</a> ",getConfiguration("url",""),getConfiguration("sitename",""));
+			$messageplain = sprintf("Link to push2press page is push2press://?url=%s ",getConfiguration("url",""),getConfiguration("sitename",""));
 			$msuccess = mail($emaillinkto, 'Push2press Email Link', $message,$headers);
 			$h = sprintf("<div> Email sent to %s %s </div>",$emaillinkto,$msuccess);
 			if ($msuccess) {
