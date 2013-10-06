21373fae2afa8653f8fd9e0055b1d5bb753cb0be
Jonathan Carter <jc@glimworm.com>
2013-10-06 16:43:11 +0200

    update to completely new menu and visual interface

--- p2p/api.js
+++ p2p/api.js
@@ -1,21 +1,23 @@
 $push2press = {};
 push2press = {};
-$push2press.loading = function() {
-	var h = '<div style="padding:16px;"><br><legend>Upgrading</legend><br><br><div id="p2p_download_bar">'+
-	'<div class="progress progress-striped active">'+
-  	'	<div class="bar" style="width: 100%;"></div>'+
-	'</div>'+
-	'</div>'+
-	'</div>';
-	$("#modal-window2").removeClass("modalpreview");
-	$("#modal-window2").removeClass("modalpreview600");
-	$("#modal-window2").removeClass("modalpreview200qrcode");
-	$("#modal-window2").addClass("modalpreview200");
-	$("#modal-window2").html(h);
-	$("#modal-window2").modal('show');
-	setTimeout(function() {
-		document.location.href='upgrade.php';
-	},500);
+push2press.loading = function() {
+	if (confirm("Are you sure?")) {
+		var h = '<div style="padding:16px;"><br><legend>Upgrading</legend><br><br><div id="p2p_download_bar">'+
+		'<div class="progress progress-striped active">'+
+	  	'	<div class="bar" style="width: 100%;"></div>'+
+		'</div>'+
+		'</div>'+
+		'</div>';
+		$("#modal-window2").removeClass("modalpreview");
+		$("#modal-window2").removeClass("modalpreview600");
+		$("#modal-window2").removeClass("modalpreview200qrcode");
+		$("#modal-window2").addClass("modalpreview200");
+		$("#modal-window2").html(h);
+		$("#modal-window2").modal('show');
+		setTimeout(function() {
+			document.location.href='upgrade.php';
+		},500);
+	}
 };
 push2press.modal = function(id) {
 	$("#modal-window2").removeClass("modalpreview");
